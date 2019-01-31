910d8fd65412516accf66cf711fa8bf33aeec718
liuzhun0910 <47011143+liuzhun0910@users.noreply.github.com>
2019-01-31 15:34:32 -0500

    Copyright Date Added

--- src/softwaredesign/week1/Bead.java
+++ src/softwaredesign/week1/Bead.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor. I am adding a date to this comment, Jan 31, 2019
  */
 package bead;
 
